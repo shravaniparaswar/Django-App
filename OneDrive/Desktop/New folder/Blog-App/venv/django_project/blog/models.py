01c22fd666a52429f55c6662cf5c3cf49035a154
shravaniparaswar <shravani.paraswar@gmail.com>
2023-01-23 18:44:07 +0530

    files

---
+++ OneDrive/Desktop/New folder/Blog-App/venv/django_project/blog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+#python manage.py sqlmigrate blog 0001
+#__ is called dunder methord ie double unddercore methord
